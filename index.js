2f42f735a68f1d26c91a9ce125bf3cee6e937343
Khiroun <em_khiroun@esi.dz>
2022-09-10 18:18:40 +0100

    send and store file via http

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const app = require("express")();
-const PORT = process.env.PORT || 5000;
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  destination: (req, file, cb) => {
+    cb(null, "./upload");
+  },
+});
+
+const upload = multer({ storage: storage });
+const PORT = process.env.PORT || 5000;
 app.get("/ws", (req, res) => {
   res.status(200).send({
     h: 2,
+  });
+});
+app.post("/ws", upload.single("word-image"), (req, res) => {
+  const file = req.file;
+
+  res.send({
+    name: file?.originalname,
   });
 });
 
